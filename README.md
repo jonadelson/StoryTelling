ab4d7651de3a4e87a697145aadae1637d9030319
jonadelson <jonathan.adelson@gmail.com>
2016-02-10 11:10:47 -0500

    more changes

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # StoryTelling
+
+Required library: tweepy
+
+---------------------------------------------------------------------------
+
+Note: Use port=8080 for websocketd because that is what index.html looks for
+
+Usage:
+websocketd --port=8080 python TwitterStreamer.py
+python -m SimpleHTTPServer 8000
+localhost:8000
+
+Wait a few seconds and the images and tweets should start to appear
+
+----------------------------------------------------------------------------
+
+In this exercise I will be consuming tweets from the Twitter Streaming API. 
+I used this website (http://adilmoujahid.com/posts/2014/07/twitter-analytics/)
+as a resource to understand how to consume tweets through the tweepy library 
+in python. I experimented with a number of different search terms and ultimately
+decided to use 'Isis' as my query. 
+
+I focused only on the tweets that had a 'media_url_https' field associated with
+them. Each individual message can represent a number of things. It can be a report
+on a current news event related to Isis. It can be one of the presidential candidates
+proclaiming they will fight terrorism and therefore invoking the name 'Isis'. I have 
+found that often it can even be a joke from someone saying that someone else's actions
+are even worse than Isis. 
+
+I expect that there will be a lot of tweets associated with this. There probably
+would have been more a few weeks ago, but it is still very much a hot topic and there
+are still plenty of news stories concerning Isis as well as presidential candidates
+invoking their name. The tweets come in fairly quickly so I make sure that I only print
+the tweets after I have collected 10 with images so that the images and text associated
+with a particular tweet remain on the webpage for long enough. 
--- TwitterStreamer.py
+++ TwitterStreamer.py
@@ -1,11 +1,13 @@
+# Code partially adapted from http://adilmoujahid.com/posts/2014/07/twitter-analytics/
+
 import json
-import sys
 import time
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from collections import Counter
 
+# Credentials
 API_KEY = '5mzm3qQTbpN82LYtdlC2iuMVE'
 API_SECRET = 'KGuyr3RiiWh3DRYPiSUq1ia2anfXiq7SAzXXhJBdymq2R2dajK'
 ACCESS_TOKEN = '3127328500-e6PNj8ltrqXBSfGkAR6LcYogFx1OSJYzX2Cxeic'
@@ -14,18 +16,30 @@ ACCESS_TOKEN_SECRET = 'x9QAzJ90gICvHi83YOa92ZOFVIpVJPJwveBIOeOE4Fp5p'
 class StdOutListener(StreamListener):
 
     def __init__(self):
-        self.out = []
-        self.count = 0
+        self.out = [] # keep list of JSON objects
+        self.count = 0 # keep count of tweets that have images attached
     
     def on_data(self, data):
+        """
+        This function says what to do with the data when it is consumed
+        in the stream. I decided to only print out tweets that had a 
+        media_url_https field so there is a check for this particular field.
+        Also, I wait until I have a list of 10 tweets before I print it out. 
+        I wait for 10 tweets because I do not want the web page refreshing too often.
+        I only send the first three tweets however because it is a bit overwhelming
+        to actually look at 10 tweets at once on the webpage. 
+        """
         decoded = json.loads(data)
-        json_object = {}
+        json_object = {} # object that will hold tweet info
+        # Check if there is a media field
         if 'media' in decoded['entities']:
             self.count += 1
+            # Collect the image url and the tweet text and add to the list
             json_object['url'] = decoded['entities']['media'][0]['media_url_https']
             json_object['text'] = decoded['text']
             self.out.append(json_object)
-            if self.count == 3:
+            # If there are 10 tweets, print them 
+            if self.count == 10:
                 print json.dumps(self.out[:3])
                 sys.stdout.flush()
                 self.out = []
@@ -33,15 +47,24 @@ class StdOutListener(StreamListener):
         return True
 
     def on_error(self, status):
+        """
+        This function simply prints the error message if one happens to occur
+        """
         print status
 
 
 if __name__ == '__main__':
 
-    l = StdOutListener()
+    l = StdOutListener() # instantiate class
+    # Set up authorizations
     auth = OAuthHandler(API_KEY, API_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    # set up the stream
     stream = Stream(auth, l)
+
+    # keyword of term to search for
     search_term = 'isis'
 
+    # start stream and filter term 
     stream.filter(track=[search_term])
